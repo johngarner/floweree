cc5800b5b07e3c0ac878c5b0e36eebc3b1294fb0
Kiet Tran <kietanhtran38@gmail.com>
2018-10-10 17:32:01 -0500

    Files from john/MVP

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Image, Alert, AppRegistry, Button, StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+export default class ButtonBasics extends Component {
+  _onPressButton() {
+    Alert.alert('Alert')
+  }
+
   render() {
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+
+        <View style={styles.flowerContainer}>
+          <Image source={require('./assets/flowers/flower1.png')}/>
+        </View>
+        
+        <View style={styles.buttonContainer1}>
+
+          <Button
+            onPress={this._onPressButton}
+            title="1"
+          />
+        </View>
+
+        <View style={styles.buttonContainer2}>
+          <Button
+            onPress={this._onPressButton}
+            title="2"
+            color="#841584"
+          />
+        </View>
+
+        <View style={styles.buttonContainer3}>
+          <Button
+            onPress={this._onPressButton}
+            title="3"
+          />
+        </View>
+
+        <View style={styles.buttonContainer4}>
+          <Button
+            onPress={this._onPressButton}
+            title="4"
+            color="#841584"
+          />
+        </View>
+
       </View>
     );
   }
@@ -13,9 +53,45 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   width: "100%",
+   height: "100%",
+   backgroundColor: "white",
   },
+  buttonContainer1: {
+    marginTop: 30,
+    marginLeft: 30,
+    left: 0,
+    top: 0,
+    backgroundColor: "red",
+    position: "absolute",
+  },
+  buttonContainer2: {
+    marginTop: 30,
+    marginRight: 30,
+    right: 0,
+    top: 0,
+    backgroundColor: "blue",
+    position: "absolute",
+  },
+  buttonContainer3: {
+    marginBottom: 30,
+    marginLeft: 30,
+    left: 0,
+    bottom: 0,
+    backgroundColor: "green",
+    position: "absolute",
+  },
+  buttonContainer4: {
+    marginRight: 30,
+    marginBottom: 30,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "purple",
+    position: "absolute",
+  },
+  flowerContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
