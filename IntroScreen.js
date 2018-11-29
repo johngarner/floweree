3e696bb21e12992169657a9918a7062f8281c52f
Kiet Tran <kietanhtran38@gmail.com>
2018-11-28 18:26:07 -0600

    animated homescreen flower

--- IntroScreen.js
+++ IntroScreen.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
-import { Alert, AppRegistry, Button, StyleSheet, Image, Text, View, TouchableOpacity, ScrollView } from 'react-native';
+import { Alert, AppRegistry, Button, StyleSheet, Text, View, TouchableOpacity, Animated, Image, Easing } from 'react-native';
 import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 
 export class IntroScreen extends React.Component {
+  constructor() {
+    super();
+    this.spinValue = new Animated.Value(0);
+  }
+
 	static navigationOptions = {
 		title: 'Intro Screen',
 		headerStyle: {
@@ -11,17 +16,42 @@ export class IntroScreen extends React.Component {
 		headerTintColor: '#fff',
 	};
 
-  	_onPressButton() {
+  _onPressButton() {
   		Alert.alert('How to play', 'Click the yellow watering can to water your flower. The flower needs to be watered once every 24 hours in order for the flower to grow.');
-	}	
+	}
+
+  componentDidMount() {
+    this.spin();
+  }
+
+  spin() {
+    this.spinValue.setValue(0);
+    Animated.timing(
+      this.spinValue,
+      {
+        toValue: 1,
+        duration: 4000,
+        easing: Easing.linear
+      }
+    ).start(() => this.spin()); // call itself again when the animation is completed, basically creating an infinite animation
+  }	
 
   render() {
+    const spin = this.spinValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg']
+    }); // for spinning animation
+
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: "#FFCB8B" }}>
-        <Image
-        	source = {require('./assets/flowers/homescreenflower.png')}
-        	style= {{width: 100, height: 100}}
+        <Animated.Image
+          style={{
+            width: 100,
+            height: 100,
+            transform: [{rotate: spin}] }}
+            source={require('./assets/flowers/homescreenflower.png')}
         />
+
         <Button
           title= "Play Game"
           onPress={() => this.props.navigation.navigate('Game')}
